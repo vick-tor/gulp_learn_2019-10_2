301dde3928ac1afc8860719c1ca24bde51db9a20
vick-tor <vick-tor69@yandex.ru>
2019-10-11 08:08:11 +0400

    Добавлены ссылки на статьи для новичков

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
-/* Gulp. Гайд для новичков
-*  https://thegazer.ru/gajd-po-gulp/
-*  https://webformyself.com/gulp-dlya-nachinayushhix/
-*   http://www.creative-seo.ru/blog/gulp-dlya-nachinayushchih/
+/* Gulp. Гайд для новичков  *  https://thegazer.ru/gajd-po-gulp/
+*  https://webformyself.com/gulp-dlya-nachinayushhix/    *   http://www.creative-seo.ru/blog/gulp-dlya-nachinayushchih/
 */
-var gulp = require('gulp');
-var plugins = require('gulp-load-plugins')();
-
-
+const gulp = require('gulp');
+const plugins = require('gulp-load-plugins')();
+const del = require('del');
+/*
 gulp.task('hello', function(){
     console.log('Hello World');
 });
@@ -18,11 +16,26 @@ gulp.task('log', function(){
 gulp.task('Three', function(){
     console.log('Say - this is therd time, is Three!');
 });
-
-
-//gulp.task('default', ['hello','log']);                - syntax 3!!!
-//gulp.task('default', gulp.parallel('hello', 'log', 'Three'));    // - syntax 4
+*/
+/*gulp.task('default', ['hello','log']);                - syntax 3!!!
+gulp.task('default', gulp.parallel('hello', 'log', 'Three'));    // - syntax 4
 gulp.task('default', gulp.parallel('Three','hello', 'log'));    // - syntax 4
-//gulp.task('default', gulp.series('hello', 'log'));
+gulp.task('default', gulp.series('hello', 'log'));
+gulp.task('default',(gulp.series( gulp.parallel('Three', 'hello'), gulp.parallel('log')))) */
 
-//gulp.task('default',(gulp.series( gulp.parallel('Three', 'hello'), gulp.parallel('log'))));
+gulp.task('move', function(){
+     return gulp.src('aaa/**/*')
+         .pipe(gulp.dest('bbb'));
+});
+
+
+gulp.task('clean', function(done) {  //syntax 4  -  ошибок нет
+    del.sync('bbb/**/*');
+    done();
+});
+
+gulp.task('cl', function() {  //syntax 3  -  чистит, но выдает ошибки
+    return del.sync('bbb/*');
+});
+
+gulp.task('default', gulp.parallel('cl'));
