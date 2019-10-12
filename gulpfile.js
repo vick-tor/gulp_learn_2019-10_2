40884be858f1b695209650cacc0e790d444dbb54
vick-tor <vick-tor69@yandex.ru>
2019-10-12 12:19:20 +0400

    ТЕМА  -  Запускаем Browser-Sync c HTML

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,21 @@
-/* Gulp. Гайд для новичков  *  https://thegazer.ru/gajd-po-gulp/
-*  https://webformyself.com/gulp-dlya-nachinayushhix/    *   http://www.creative-seo.ru/blog/gulp-dlya-nachinayushchih/
-*  https://toster.ru/q/594171
-*/
+/* Gulp. Гайд для новичков  в комите 6-7 */
+
 const gulp = require('gulp');
 const plugins = require('gulp-load-plugins')();
 const del = require('del');
+const browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
 
-//ТЕМА  -  1.копирование и удаление фалов,  2.составление ПУТЕЙ
+//ТЕМА  -  Запускаем Browser-Sync
 
-
-var path = {
-    delfile:{ del:'dist/**' },
-    js:{
-        jsa: 'aaa/js/*.js',
-        jsb: 'bbb/js/*.js'
+gulp.task('html', function(){
+    return gulp.src('*.html')
+        .pipe(gulp.dest('dist'))
+        .pipe(browserSync.reload({stream: true}));
     }
-};  // 1.Прописываем пути обращения к папкам (в этом примере - для копирования и удаления)
-gulp.task('copya', function() {
-        return gulp.src(path.js.jsa)
-            .pipe(gulp.dest('dist/js'));
-    }
-);  //  2.Прописываем функцию копирования с путем обращения к папке ааа
-gulp.task('copyb', function() {
-        return gulp.src(path.js.jsb)
-            .pipe(gulp.dest('dist/js'));
-    }
-);  //  3.Прописываем функцию копирования с путем обращения к папке bbb
-gulp.task('copyab', gulp.parallel('copya', 'copyb')); //  4.Пишем функцию параллельного исполнения двух задач !!! И пишем именно так, а то не сработает!!!
+);
 
-
-gulp.task('del', function(done){
-    del.sync(path.delfile.del);
-    done();
+gulp.task('run', function(){
+        browserSync.init({server:'dist'});
+        gulp.watch('dist/**/*').on('change', browserSync.reload);
 });
-
-
-gulp.task('clean', function(done) {  //syntax 4  -  ошибок нет
-    del.sync('dist');
-    done();
-});
-
-gulp.task('cl', function() {  //syntax 3  -  чистит, но выдает ошибки
-    return del.sync('bbb/*');
-});
-
-gulp.task('default', gulp.parallel('cl'));
-
-
-
-
-/*
- gulp.task('hello', function(){
- console.log('Hello World');
- });
-
- gulp.task('log', function(){
- console.log('Hello World again!');
- });
-
- gulp.task('Three', function(){
- console.log('Say - this is therd time, is Three!');
- });
- */
-/*gulp.task('default', ['hello','log']);                - syntax 3!!!
- gulp.task('default', gulp.parallel('hello', 'log', 'Three'));    // - syntax 4
- gulp.task('default', gulp.parallel('Three','hello', 'log'));    // - syntax 4
- gulp.task('default', gulp.series('hello', 'log'));
- gulp.task('default',(gulp.series( gulp.parallel('Three', 'hello'), gulp.parallel('log')))) */
